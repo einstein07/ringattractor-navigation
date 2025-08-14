7931910b4df356160aac3df9bd99aa9301d22b5b
Sindiso <mkhsin035@myuct.ac.za>
2025-08-14 11:47:17 +0200

    Updated movement parameters

--- agile_flexible_navigation/control.py
+++ agile_flexible_navigation/control.py
@@ -205,9 +205,9 @@ class ViconSubscriber(Node):
                         msg.positions[i].y_rot, 
                         msg.positions[i].z_rot)))
                 
-            #self.get_logger().info('subject "%s" with segment %s:' %(msg.positions[i].subject_name, msg.positions[i].segment_name))
-            #self.get_logger().info('I heard translation in x, y, z: "%f", "%f", "%f"' % (msg.positions[i].x_trans, msg.positions[i].y_trans, msg.positions[i].z_trans))
-            #self.get_logger().info('I heard rotation in x, y, z, w: "%f", "%f", "%f", "%f": ' % (msg.positions[i].x_rot, msg.positions[i].y_rot, msg.positions[i].z_rot, msg.positions[i].w))
+            self.get_logger().info('subject "%s" with segment %s:' %(msg.positions[i].subject_name, msg.positions[i].segment_name))
+            self.get_logger().info('I heard translation in x, y, z: "%f", "%f", "%f"' % (msg.positions[i].x_trans, msg.positions[i].y_trans, msg.positions[i].z_trans))
+            self.get_logger().info('I heard rotation in x, y, z, w: "%f", "%f", "%f", "%f": ' % (msg.positions[i].x_rot, msg.positions[i].y_rot, msg.positions[i].z_rot, msg.positions[i].w))
 
     
     #======================define move_to function=========================
@@ -320,7 +320,7 @@ class ViconSubscriber(Node):
 
         pos_message = {}
 
-        time.sleep(1)
+        #time.sleep(1)
 
         if turn_back:
             self.move_to(next_x, next_y, 1)
